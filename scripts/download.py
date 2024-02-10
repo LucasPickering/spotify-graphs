f8cb6b87ccac0339421e41b9438630fcaf1b440d
Lucas Pickering <lucas@lucaspickering.me>
2024-02-09 20:27:25 -0500

    Download much more data

--- scripts/download.py
+++ scripts/download.py
@@ -1,44 +1,88 @@
 #!/usr/bin/env python3
 
 import argparse
+import asyncio
 import itertools
 import json
 from typing import Any
 
 import httpx
+from tqdm.asyncio import tqdm
+
+MAX_SEARCH_SIZE = 50
+MAX_ARTIST_SIZE = 50
+TRACK_KEYS = {"id", "genres", "duration_ms", "name"}
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--creds", default="creds.json")
     parser.add_argument("--output", "-o", default="data/tracks.json")
+    parser.add_argument("--search-terms", default="search_terms.txt")
     args = parser.parse_args()
 
     with open(args.creds) as f:
         creds = json.load(f)
 
+    with open(args.search_terms) as f:
+        search_terms = f.readlines()
+
     # Exchange creds for a temporary access token
     access_token = get_access_token(creds)
+    tracks = asyncio.run(download_tracks(access_token, search_terms))
 
-    with httpx.Client(
+    with open(args.output, "w") as f:
+        json.dump(tracks, f)
+
+
+async def download_tracks(
+    access_token: str, search_terms: list[str]
+) -> list[dict[str, Any]]:
+    async with httpx.AsyncClient(
         base_url="https://api.spotify.com",
         headers={"Authorization": f"Bearer {access_token}"},
     ) as client:
-        tracks = search(client, "test")
+        # First, run each search term and take the first page of results
+        print("Downloading tracks")
+        tracks = [
+            track
+            for tracks in await tqdm.gather(
+                *(search(client, query) for query in search_terms),
+                total=len(search_terms),
+            )
+            for track in tracks
+        ]
+
         # Grab all the artist IDs so we can do a separate request to get genres
-        artist_ids = set(
-            artist["id"] for track in tracks for artist in track["artists"]
+        artist_ids = {artist["id"] for track in tracks for artist in track["artists"]}
+
+        print("Downloading artists")
+        artists = itertools.chain.from_iterable(
+            await tqdm.gather(
+                *(
+                    download_artists(client, ids)
+                    for ids in chunked(artist_ids, MAX_ARTIST_SIZE)
+                )
+            )
         )
-        genres = get_genres(client, list(artist_ids))
+
+    # Map each artist to their genres
+    artist_genres = {artist["id"]: artist["genres"] for artist in artists}
 
     # Join tracks with their genres
+    keys_to_delete = set(tracks[0].keys()) - TRACK_KEYS
     for track in tracks:
         track["genres"] = list(
-            {genre for artist in track["artists"] for genre in genres[artist["id"]]}
+            {
+                genre
+                for artist in track["artists"]
+                for genre in artist_genres[artist["id"]]
+            }
         )
+        for key in keys_to_delete:
+            del track[key]
 
-    with open(args.output, "w") as f:
-        json.dump(tracks, f, indent=4)
+    return tracks
 
 
 def get_access_token(creds):
@@ -55,27 +99,26 @@ def get_access_token(creds):
     return response.json()["access_token"]
 
 
-def search(client: httpx.Client, query: str) -> list[dict[str, Any]]:
-    response = client.get(
-        "/v1/search", params={"type": "track", "q": query, "limit": 50}
+async def search(client: httpx.AsyncClient, query: str) -> list[dict[str, Any]]:
+    response = await client.get(
+        "/v1/search", params={"type": "track", "q": query, "limit": MAX_SEARCH_SIZE}
     )
     response.raise_for_status()
     return response.json()["tracks"]["items"]
 
 
-def get_genres(client: httpx.Client, artists: list[str]) -> dict[str, list[str]]:
-    mapping = {}
-    for chunk in chunked(artists, 50):
-        response = client.get("/v1/artists", params={"ids": ",".join(chunk)})
-        response.raise_for_status()
-        for artist in response.json()["artists"]:
-            mapping[artist["id"]] = artist["genres"]
-    return mapping
+async def download_artists(
+    client: httpx.AsyncClient, ids: list[str]
+) -> dict[str, list[str]]:
+    response = await client.get("/v1/artists", params={"ids": ",".join(ids)})
+    response.raise_for_status()
+    return response.json()["artists"]
 
 
-def chunked(l, n):
-    for i in range(0, len(l), n):
-        yield l[i : i + n]
+def chunked(iterable, n):
+    it = iter(iterable)
+    while batch := tuple(itertools.islice(it, n)):
+        yield batch
 
 
 if __name__ == "__main__":
